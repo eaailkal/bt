c84d04da195d1670b9195680bd45406ab2873b56
Ilya Kalinkin <ilya@Ilyas-MacBook-Pro.local>
2017-03-06 16:52:26 +0530

    adding main pages and images

--- registration.php
+++ registration.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Tyacon 2017 -- Home</title>
+    <title>Tyacon 2017 -- Registration</title>
 		<link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/mystyles.css" rel="stylesheet">
   </head>
-  <body id="home">
+  <body id="registration">
 
     <section class="container">
       <div class="content row">
