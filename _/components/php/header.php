ce33d2a3c00ec421f41fa3bfc43491911b3f1b73
Ilya Kalinkin <ilya@Ilyas-MacBook-Pro.local>
2017-03-06 18:09:44 +0530

    header and navigation

---
+++ _/components/php/header.php
@@ -0,0 +1,36 @@
+<div class="content row">
+	<div class="col-lg-12">
+		<header class="clearfix">
+			<section id="branding">
+				<a href="index.php"><img src="img/misc/logo.png" alt="Logo for Tyacon 2017"></a>
+			</section><!-- branding -->
+
+			<section class="navbar">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php">Home</a></li>
+				  	<!-- drop down menu -->
+				  	<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Conference <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+					    	<li><a tabindex="-1" href="#">Abstract Submission</a></li>
+					        <li><a tabindex="-1" href="registration.php">Registration</a></li>
+					        <li><a tabindex="-1" href="#">Terms and Conditions</a></li>
+					        <li class="divider"></li>
+					        <li><a tabindex="-1" href="#">Organizing commitee</a></li>
+					        <li><a tabindex="-1" href="#">Faculty</a></li>
+                      		<li><a tabindex="-1" href="#">Brochure</a></li>
+					    </ul>
+					</li><!-- drop down menu -->
+			<li><a href="program.php">Program</a></li>
+            <li><a href="venue.php">Venue</a></li>
+            <li><a href="accomodation.php">Accomodation</a></li>
+            <li><a href="events.php">Events</a></li>
+            <li><a href="getinvolved.php">Get involved</a></li>
+            <li><a href="contact.php">Contact</a></li>
+			</ul>
+			</section><!-- navbar -->
+
+
+		</header><!-- header -->
+	</div><!-- column -->
+</div><!-- content -->
--- index.php
+++ index.php
@@ -15,7 +15,9 @@
       <div class="content row">
 
         <section class="main col col-lg-8">
-          <h2>Main</h2>
+
+          <?php include "_/components/php/header.php"; ?>
+          
         </section><!-- main -->
 
         <section class="sidebar col col-lg-8">
