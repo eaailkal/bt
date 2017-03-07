22f75fd2ccafce236b9268676393b5937b88b4b5
Ilya Kalinkin <ilya@Ilyas-MacBook-Pro.local>
2017-03-07 21:32:55 +0530

    accent color

--- _/components/php/aside-faculty.php
+++ _/components/php/aside-faculty.php
@@ -2,5 +2,5 @@
 	<h2>About the faculty</h2>
 	<p>Each Featured speaker has an opportunity at the conference to share his or her vision, perspective, and techniques with conference attendees. To get more information about the faculty click on one of their photos, or visit our <a href="faculty.php">faculty</a> page.</p>
 	  <?php include "snippet-facultyphotogrid.php"; ?>
-	<a href="faculty.php" class="btn btn-warning">about the faculty</a>
+	<a href="faculty.php" class="btn btn-danger">about the faculty</a>
 </aside>
